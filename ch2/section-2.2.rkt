0bf587c36fb234642a07b613d2906816ae8d6e43
James Avery <j-a-avery@users.noreply.github.com>
2022-04-23 22:45:13 -0500

    (Almost) Finished 2.2.2

--- ch2/section-2.2.rkt
+++ ch2/section-2.2.rkt
@@ -103,3 +103,85 @@
       (newline)
       (cdr (cons (effect (car items)) ; I feel like this is cheating
                  (for-each effect (cdr items))))))
+
+;;;;;; 2.2.2 Hierarchical Structures
+
+;; Exercise 2.24
+(list 1 (list 2 (list 3 4))) ; => '(1 (2 (3 4)))
+;; See also section-2.2-exercise-2.24.png
+
+;; Exercise 2.25
+(car (cdr (car (cdr (cdr '(1 3 (5 7) 9))))))
+(car (car '((7))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr '(1 (2 (3 (4 (5 (6 7))))))))))))))))))
+
+;; Exercise 2.26
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y) ; => '(1 2 3 4 5 6)
+(cons x y) ; => '((1 2 3) . (4 5 6))
+           ; => '((1 2 3) . (4 . (5 . (6 . nil)))) ;
+           ; => '((1 2 3) 4 5 6)
+(list x y) ; => (cons x (cons y nil))
+           ; => (x . (y . nil))
+           ; => ((1 2 3) . ((4 5 6) . nil))
+           ; => '((1 2 3) (4 5 6))
+
+;; Excercise 2.27
+(define (deep-reverse lst)
+  (define (deep-reverse* lst sofar)
+    (cond ((empty? lst) sofar)
+          ((not (pair? lst)) lst)
+          (else 
+           (deep-reverse* (deep-reverse* (cdr lst) '())
+                          (cons (deep-reverse* (car lst) '()) sofar)))))
+  (deep-reverse* lst nil))
+
+;; Exercise 2.28
+(define (fringe t)
+  (cond ((empty? t) t)
+        ((not (pair? t)) t)
+        (else
+         (if (pair? (car t))
+             (append (fringe (car t))
+                     (fringe (cdr t)))
+             (cons (car t)
+                   (fringe (cdr t)))))))
+
+(fringe (list (list 1 2) (list 3 4)))
+
+;; Exercise 2.29
+;;;;;;; I remember doing this one before. I remember it being annoying.
+;;;;;;; I think I remember seeing my answers for it recently.
+;;;;;;; If I see those answers again, then I'll add them to this.
+;;;;;;; Otherwise, tacet
+
+;;;; Mapping over trees
+
+;; Exercise 2.30
+(define (square-tree1 t)
+  (cond ((null? t) nil)
+        ((not (pair? t))
+         (square t))
+        (else
+         (cons (square-tree1 (car t))
+               (square-tree1 (cdr t))))))
+
+(define (square-tree2 tree)
+  (map (lambda (subtree)
+         (if (pair? subtree)
+             (square-tree2 subtree)
+             (square subtree)))
+  tree))
+
+;; Exercise 2.31
+(define (tree-map function tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree))
+         (function tree))
+        (else
+         (cons (tree-map function (car tree))
+               (tree-map function (cdr tree))))))
+
+(define (square-tree3 tree) (tree-map square tree))
