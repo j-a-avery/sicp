d0c576a18a5d341173c8b9c25ccde6ab3b7c7f98
James Avery <j-a-avery@users.noreply.github.com>
2022-04-23 20:42:18 -0500

    Finished 2.2.1

---
+++ ch2/section-2.2-exercise-2.19.rkt
@@ -0,0 +1,41 @@
+;; Exercise 2.19
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+(define tw-coins (list 50 20 10 5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+         ((or (< amount 0)
+              (no-more? coin-values)) 0)
+         (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values))
+                 coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (empty? coin-values))
+
+;;;; Changing the order of coin-values does not affect the answer produced,
+;;;; but it does affect the amount of time it takes.
+;;;; (cc 111 uk-coins) (cc 111 uk-coins-reversed) (cc 111 uk-coins-shuffled)
+;;;; all produce the same answer, 166880, but using the shuffled list takes
+;;;; slightly longer, and using the reversed list takes four times as long.
+;;;; I feel like I should be able to prove why somehow.
+(define uk-coins-reversed (reverse uk-coins))
+(define uk-coins-shuffled (shuffle uk-coins))
+
+
+(for ([i (range 201)])
+  (for ([times (range 5)])
+    (for ([coin-list (list (cons "DSC" uk-coins) (cons "ASC" uk-coins-reversed) (cons "RND" uk-coins-shuffled))])
+      (display (apply string-append (list (number->string i) " " (car coin-list) " ")))
+      (time (display (cc i (cdr coin-list)))))))
--- ch2/section-2.2.rkt
+++ ch2/section-2.2.rkt
@@ -1,5 +1,7 @@
 #lang racket
+(define nil null)
 
+;;;; Section 2.2
 ;; Exercise 2.17
 (define (last-pair lst)
   (if (empty? (cdr lst))
@@ -16,42 +18,88 @@
       lst
       (reverse* (cdr lst) (list (car lst)))))
 
-;; Exercise 2.19
-(define us-coins (list 50 25 10 5 1))
-(define uk-coins (list 100 50 20 10 5 2 1 0.5))
-(define tw-coins (list 50 20 10 5))
+;; Exercise 2.19 - see section-2.2-exercise-2.19.rkt, etc
 
-(define (cc amount coin-values)
-  (cond ((= amount 0) 1)
-         ((or (< amount 0)
-              (no-more? coin-values)) 0)
-         (else
-          (+ (cc amount
-                 (except-first-denomination coin-values))
-             (cc (- amount
-                    (first-denomination coin-values))
-                 coin-values)))))
+;; Exercise 2.20
+;;;;;;; The easiest solution is just to use filter...
+(define (same-parity-easy example . others)
+  (if (even? example)
+      (filter even? others)
+      (filter odd? others)))
 
-(define (first-denomination coin-values)
-  (car coin-values))
+;;;;;;; but I feel like since we're talking about car and cdr, a more...
+;;;;;;; basic?... solution is intended.
+(define (same-parity example . others)
+  (define (same-parity* parity others)
+    (cond ((empty? others) others)
+          ((parity (car others))
+           (cons (car others) (same-parity* parity (cdr others))))
+          (else
+           (same-parity* parity (cdr others)))))
+  (cons example (same-parity* (if (even? example) even? odd?) others)))
 
-(define (except-first-denomination coin-values)
-  (cdr coin-values))
+;;; Mapping over lists
 
-(define (no-more? coin-values)
-  (empty? coin-values))
+;; Exercise 2.21
+(define (square x) (* x x))
 
-;;;; Changing the order of coin-values does not affect the answer produced,
-;;;; but it does affect the amount of time it takes.
-;;;; (cc 111 uk-coins) (cc 111 uk-coins-reversed) (cc 111 uk-coins-shuffled)
-;;;; all produce the same answer, 166880, but using the shuffled list takes
-;;;; slightly longer, and using the reversed list takes four times as long.
-(define uk-coins-reversed (reverse uk-coins))
-(define uk-coins-shuffled (shuffle uk-coins))
+(define (square-list1 items)
+  (if (null? items)
+      nil
+      (cons
+       (square (car items))
+       (square-list1 (cdr items)))))
+
+(define (square-list2 items)
+  (map square items))
+
+;; Exercise 2.22
+(define (square-list3 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
+
+; Examine using substitution
+; (square-list '(1 2 3 4 5))
+; => (iter '(1 2 3 4 5) '())
+; => (iter '(2 3 4 5) (1 . '()))
+; => (iter '(3 4 5) (4 . (1 . '())))
+; ...
+; => '(25 16 9 4 1)
+; 
+; This does not work because we are cons-ing the most recent element to the
+; front of the answer list.
 
 
-(for ([i (range 201)])
-  (for ([times (range 5)])
-    (for ([coin-list (list (cons "DSC" uk-coins) (cons "ASC" uk-coins-reversed) (cons "RND" uk-coins-shuffled))])
-      (display (apply string-append (list (number->string i) " " (car coin-list) " ")))
-      (time (display (cc i (cdr coin-list)))))))
+(define (square-list4 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer
+                    (square (car things))))))
+  (iter items nil))
+
+; Examine using substitution
+; (square-list '(1 2 3 4 5))
+; => (iter '(1 2 3 4 5) nil)
+; => (iter '(2 3 4 5) (nil . 1))
+; => (iter '(3 4 5) ((nil . 1) . 4))
+; => (iter '(4 5) (((nil . 1) . 4) . 9))
+; ...
+; => (((((nil . 1) . 4) . 9) . 16) . 25)
+; 
+; This does not built a list - instead of nil being the last element,
+; everything is cons-ed onto nil.
+
+
+;; Exercise 2.23
+(define (for-each effect items)
+  (if (empty? items)
+      (newline)
+      (cdr (cons (effect (car items)) ; I feel like this is cheating
+                 (for-each effect (cdr items))))))
